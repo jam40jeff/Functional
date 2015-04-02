4f7490ad7f0645ad7ae15a1ea6b40d17f01ab1eb
Jeffrey Morse <jam40jeff@gmail.com>
2015-04-02 09:03:04 -0400

    Reduced number of parameters supported.

--- Source/MorseCode.Functional/_Root/IVariant.cs
+++ Source/MorseCode.Functional/_Root/IVariant.cs
@@ -1,13 +1,5 @@
 ﻿// Generated helper templates
 // Generated items
-// MorseCode.Functional\_Root\IVariant{T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,T15,T16}.cs
-// MorseCode.Functional\_Root\IVariant{T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,T15}.cs
-// MorseCode.Functional\_Root\IVariant{T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14}.cs
-// MorseCode.Functional\_Root\IVariant{T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13}.cs
-// MorseCode.Functional\_Root\IVariant{T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12}.cs
-// MorseCode.Functional\_Root\IVariant{T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11}.cs
-// MorseCode.Functional\_Root\IVariant{T1,T2,T3,T4,T5,T6,T7,T8,T9,T10}.cs
-// MorseCode.Functional\_Root\IVariant{T1,T2,T3,T4,T5,T6,T7,T8,T9}.cs
 // MorseCode.Functional\_Root\IVariant{T1,T2,T3,T4,T5,T6,T7,T8}.cs
 // MorseCode.Functional\_Root\IVariant{T1,T2,T3,T4,T5,T6,T7}.cs
 // MorseCode.Functional\_Root\IVariant{T1,T2,T3,T4,T5,T6}.cs
